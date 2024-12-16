221d2520702bf5e48dd71edc8b2f817bff16e298
HEKEH <hekai02@163.com>
2024-12-16 11:17:06 +0800

    feat: optimize readme

---
+++ packages/player-vue/scripts/copy-readme.ts
@@ -0,0 +1,20 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+const rootDir = path.join(__dirname, '../../../');
+const readmeFiles = ['./README.md', './README.zh-cn.md'];
+
+readmeFiles.forEach(file => {
+  const filePath = path.join(rootDir, file);
+  const targetPath = path.join(__dirname, '..', file);
+  let content = fs.readFileSync(filePath, 'utf8');
+  content = content.replace(
+    /\((img|\.\/img)\/(.+?)\)/g,
+    '(https://raw.githubusercontent.com/HEKEH/easy-audio-player/master/img/$2)',
+  );
+  content = content.replace(
+    /\.?\((README(?:\.[\w-]+)?\.md)\)/g,
+    '(https://www.github.com/HEKEH/easy-audio-player/blob/master/$1)',
+  );
+  fs.writeFileSync(targetPath, content);
+});
